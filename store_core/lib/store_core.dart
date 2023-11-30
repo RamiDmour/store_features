9f62aac6161af2d3dcf4afbc7988162ab2dc049e
Rami Dmaur <timurwelike@gmail.com>
2023-11-30 12:07:11 +0300

    add publish info

--- store_core/lib/store_core.dart
+++ store_core/lib/store_core.dart
@@ -1,7 +1,5 @@
 library store_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+void greetFromCore() {
+  print("Hello from Core!");
 }
